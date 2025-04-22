46cff2c492ce1cd8758493456404fe8a30fc40c8
Siyabonga Zungu <52372746+216037150@users.noreply.github.com>
2025-04-22 07:55:06 +0200

    Update guess.js
    added try{}catch{}

--- guess.js
+++ guess.js
@@ -1,16 +1,23 @@
 let n = Math.floor(Math.random() * 4) + 1;
-console.log(n); // For debugging purposes; 
-let guess = parseInt(prompt('Enter the number: '));
+
+let guess;
 
 while (n !== guess) {
-    if (isNaN(guess)) {
-        console.log("Enter a valid number.");
-    } else if (guess < n) {
-        console.log("The number is too low");
-    } else if (guess > n) {
-        console.log("The number is too big");
+    try {
+        guess = parseInt(prompt('Enter the number: '));
+
+        if (isNaN(guess)) {
+            throw new Error("Invalid input");
+        }
+
+        if (guess < n) {
+            console.log("The number is too low");
+        } else if (guess > n) {
+            console.log("The number is too big");
+        }
+    } catch (error) {
+        console.log("Please enter a valid number.");
     }
-    guess = parseInt(prompt('Enter the number: '));
 }
 
 console.log("Congratulations, you guessed correct!!!");
